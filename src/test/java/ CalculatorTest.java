3bdf3ad71d1a3d9ba4992980444665d155f90ab0
AlexGeorgievich <alexhome1@yandex.ru>
2024-12-27 16:30:26 +0300

    добавлен Allure Reports for tests

--- src/test/java/ CalculatorTest.java
+++ src/test/java/ CalculatorTest.java
@@ -1,39 +1,48 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.example.Calculator;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Feature("Calculator")
 class CalculatorTest {
 
     @Test
-    @DisplayName("Проверка операции сложения ")
+    @Description("Test addition operation")
+    @Story("Addition")
     public void testAddition() throws Exception {
         assertEquals("3", Calculator.calculate("1 + 2"));
         assertEquals("10", Calculator.calculate("5 + 5"));
     }
 
     @Test
-    @DisplayName("Проверка операции вычитания ")
+    @Description("Test subtraction operation")
+    @Story("Subtraction")
     public void testSubtraction() throws Exception {
         assertEquals("1", Calculator.calculate("2 - 1"));
         assertEquals("5", Calculator.calculate("10 - 5"));
     }
 
-    @DisplayName("Проверка операции умножения ")
     @Test
+    @Description("Test multiplication operation")
+    @Story("Multiplication")
     public void testMultiplication() throws Exception {
         assertEquals("6", Calculator.calculate("2 * 3"));
         assertEquals("10", Calculator.calculate("2 * 5"));
     }
 
     @Test
+    @Description("Test division operation")
+    @Story("Division")
     public void testDivision() throws Exception {
         assertEquals("2", Calculator.calculate("6 / 3"));
         assertEquals("2", Calculator.calculate("10 / 5"));
     }
 
     @Test
-    @DisplayName("Проверка операции вычитания ")
+    @Description("Test invalid input")
+    @Story("Invalid Input")
     public void testInvalidInput() {
         Exception exception = assertThrows(Exception.class, () -> {
             Calculator.calculate("1 + 2 1");
@@ -42,6 +51,8 @@ class CalculatorTest {
     }
 
     @Test
+    @Description("Test invalid operation")
+    @Story("Invalid Operation")
     public void testInvalidOperation() {
         Exception exception = assertThrows(Exception.class, () -> {
             Calculator.calculate("1 % 2");
@@ -50,6 +61,8 @@ class CalculatorTest {
     }
 
     @Test
+    @Description("Test out of range input")
+    @Story("Out of Range")
     public void testOutOfRange() {
         Exception exception = assertThrows(Exception.class, () -> {
             Calculator.calculate("11 + 2");
@@ -58,8 +71,8 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("Проверка операции деления на 0 ")
-
+    @Description("Test division by zero")
+    @Story("Division by Zero")
     public void testDivisionByZero() {
         Exception exception = assertThrows(Exception.class, () -> {
             Calculator.calculate("1 / 0");
